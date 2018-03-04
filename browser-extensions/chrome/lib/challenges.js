dad6b19de23718f8507673dd173e293703ca193c
fraz3alpha <andrewtaylor2@gmail.com>
2018-03-04 17:52:35 +0000

    Add more letters challenges, and words ones
      - Refactored alphabeteer challenge to be generic to include
        any arbitrary set of letters
      - Added a function to look for specific words in the parkrun name

--- browser-extensions/chrome/lib/challenges.js
+++ browser-extensions/chrome/lib/challenges.js
@@ -8,17 +8,176 @@ function challenge_generate_data(results) {
     return {
         "tourist": challenge_tourist(results, "tourist", "Tourist", 20),
         "cowell-club": challenge_tourist(results, "cowell-club", "Cowell Club", 100),
-        "alphabeteer": challenge_alphabeteer(results),
+        "alphabeteer": challenge_start_letters(results, "alphabeteer", "Alphabeteer", ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]),
         "fiver": challenge_single_parkrun_count(results, "fiver", "Fiver", 5),
         "tenner": challenge_single_parkrun_count(results, "tenner", "Tenner", 10),
         "single-ton": challenge_single_parkrun_count(results, "single_ton", "Single-Ton", 100),
         "double-ton": challenge_single_parkrun_count(results, "double_ton", "Double-Ton", 200),
-        "stopwatch-bingo": challenge_stopwatch_bingo(results)
-        // "pirates": challenge_pirates(results)
+        "stopwatch-bingo": challenge_stopwatch_bingo(results),
+        "pirates": challenge_start_letters(results, "pirates", "Pirates!", ["c","c","c","c","c","c","c","r"]),
+        "stayin-alive": challenge_start_letters(results, "stayin-alive", "Stayin' Alive", ["b","b","b","g","g","g"]),
+        "compass-club": challenge_words(results, "compass-club", "Compass Club", ["north","south","east","west"]),
     }
 }
 
-// Complete 20 different parkruns
+function challenge_start_letters(results, shortname, longname, letters_array) {
+    complete = false
+    completed_on = null
+    subparts = []
+    subparts_completed_count = 0
+    subparts_detail = []
+
+    // Add all the subparts to the list
+    letters_array.forEach(function (letter) {
+        // Store each one as the parts we need to do
+        subparts.push(letter)
+        // Create placeholders for each contributing result
+        subparts_detail.push(null)
+    })
+
+    checked_parkruns = []
+
+    results.parkruns_completed.forEach(function (parkrun_event) {
+
+        if (!(checked_parkruns.includes(parkrun_event.name))) {
+            initial_letter = parkrun_event.name[0].toLowerCase()
+            // Skips those parkruns that aren't going to match at all
+            if (subparts.includes(initial_letter)) {
+                // Loop through all the letters we are looking for
+                for (i=0; i<subparts.length; i++) {
+                    // Find a matching subpart that hasn't yet been filled in
+                    if (subparts_detail[i] == null && subparts[i] == initial_letter) {
+                        // Add the event
+                        p = Object.create(parkrun_event)
+                        p.subpart = initial_letter
+                        p.info = p.date
+                        subparts_detail[i] = p
+                        subparts_completed_count += 1
+
+                        if (subparts.length == subparts_completed_count) {
+                            complete = true
+                            completed_on = p.date
+                        }
+                        // Get out of the for loop
+                        break
+                    }
+                }
+            }
+
+            // Lets not process this parkrun again, even if we have run it more than once
+            checked_parkruns.push(parkrun_event.name)
+        }
+    })
+
+    // Add in all the missing ones
+    for (i=0; i< subparts.length; i++) {
+        if (subparts_detail[i] == null) {
+            subparts_detail[i] = {
+                "subpart": subparts[i],
+                "info": "-"
+            }
+        }
+    }
+
+    // Return an object representing this challenge
+    return {
+        "shortname": shortname,
+        "name": longname,
+        "complete": complete,
+        "completed_on": completed_on,
+        "subparts": subparts,
+        "subparts_count": subparts.length,
+        "subparts_detail": subparts_detail,
+        "subparts_completed_count": subparts_completed_count
+    }
+}
+
+function challenge_words(results, shortname, longname, word_array) {
+    complete = false
+    completed_on = null
+    subparts = []
+    subparts_completed_count = 0
+    subparts_detail = []
+
+    // Add all the subparts to the list
+    word_array.forEach(function (word) {
+        // Store each one as the parts we need to do
+        subparts.push(word.toLowerCase())
+        // Create placeholders for each contributing result
+        subparts_detail.push(null)
+    })
+
+    checked_parkruns = []
+
+    results.parkruns_completed.forEach(function (parkrun_event) {
+
+        if (!(checked_parkruns.includes(parkrun_event.name))) {
+            // Loop through all the words we are looking for
+            for (i=0; i<subparts.length; i++) {
+                // Find a matching subpart that hasn't yet been filled in
+                if (subparts_detail[i] == null && parkrun_event.name.toLowerCase().indexOf(subparts[i]) != -1) {
+                    // Add the event
+                    p = Object.create(parkrun_event)
+                    p.subpart = subparts[i]
+                    p.info = p.date
+                    subparts_detail[i] = p
+                    subparts_completed_count += 1
+
+                    if (subparts.length == subparts_completed_count) {
+                        complete = true
+                        completed_on = p.date
+                    }
+                    // Get out of the for loop
+                    break
+                }
+            }
+
+
+            // Lets not process this parkrun again, even if we have run it more than once
+            checked_parkruns.push(parkrun_event.name)
+        }
+    })
+
+    // Add in all the missing ones
+    for (i=0; i< subparts.length; i++) {
+        if (subparts_detail[i] == null) {
+            subparts_detail[i] = {
+                "subpart": subparts[i],
+                "info": "-"
+            }
+        }
+    }
+
+    // Return an object representing this challenge
+    return {
+        "shortname": shortname,
+        "name": longname,
+        "complete": complete,
+        "completed_on": completed_on,
+        "subparts": subparts,
+        "subparts_count": subparts.length,
+        "subparts_detail": subparts_detail,
+        "subparts_completed_count": subparts_completed_count
+    }
+}
+
+function group_results_by_event(results) {
+
+    events = {}
+
+    results.parkruns_completed.forEach(function (parkrun_event) {
+        // Create an empty list if we haven"t seen this parkrun before
+        if (!(parkrun_event.name in events)) {
+            events[parkrun_event.name] = []
+        }
+        // Append this instance to the appropriate event list
+        events[parkrun_event.name].push(parkrun_event)
+    });
+
+    return events
+}
+
+// Complete x different parkruns (20 and 100 are standard)
 function challenge_tourist(results, shortname, longname, count) {
     complete = false
     completed_on = null
@@ -69,70 +228,6 @@ function challenge_tourist(results, shortname, longname, count) {
     }
 }
 
-function challenge_alphabeteer(results) {
-    shortname = "alphabeteer"
-    longname = "Alphabeteer"
-    complete = false
-    completed_on = null
-    subparts = []
-    subparts_completed_count = 0
-    subparts_detail = []
-    include_missing = true
-
-    // Add all the subparts to the list
-    for (i=0; i<26; i++) {
-        letter = "abcdefghijklmnopqrstuvwxyz"[i]
-        subparts.push(letter)
-        subparts_detail[letter] = null
-    }
-
-    results.parkruns_completed.forEach(function (parkrun_event) {
-        // Find the first letter of the parkrun for the alphabet challenge
-        first_letter = parkrun_event.name[0].toLowerCase()
-        subparts_detail_index = "abcdefghijklmnopqrstuvwxyz".indexOf(first_letter)
-        if (subparts_detail_index != -1) {
-            if (subparts_detail[subparts_detail_index] == null) {
-                subparts_detail[subparts_detail_index] = Object.create(parkrun_event)
-                subparts_detail[subparts_detail_index].subpart = first_letter
-                subparts_detail[subparts_detail_index].info = parkrun_event.date
-                subparts_completed_count += 1
-                // Work out if this challenge has been completed by comparing the number
-                // of subparts defined to the number of subparts complete
-                if (subparts.length == subparts_completed_count) {
-                    complete = true
-                    completed_on = parkrun_event.date
-                }
-            }
-        }
-
-
-    });
-
-    // Add in all the missing ones
-    for (i=0; i< subparts_detail.length; i++) {
-        if (subparts_detail[i] == null) {
-            subparts_detail[i] = {
-                "subpart": subparts[i],
-                "info": "-"
-            }
-        }
-    }
-
-
-
-    // Return an object representing this challenge
-    return {
-        "shortname": shortname,
-        "name": longname,
-        "complete": complete,
-        "completed_on": completed_on,
-        "subparts": subparts,
-        "subparts_count": subparts.length,
-        "subparts_detail": subparts_detail,
-        "subparts_completed_count": subparts_completed_count
-    }
-}
-
 function challenge_stopwatch_bingo(results) {
     shortname = "stopwatch-bingo"
     longname = "Stopwatch Bingo"
@@ -156,7 +251,6 @@ function challenge_stopwatch_bingo(results) {
     results.parkruns_completed.forEach(function (parkrun_event) {
         // Take the last 2 characters of the time
         seconds = parkrun_event.time.substr(parkrun_event.time.length - 2)
-        console.log(seconds)
         // Convert them to a number to get the index in our array
         subparts_detail_index = parseInt(seconds)
         if (subparts_detail[subparts_detail_index] == null) {
@@ -199,7 +293,7 @@ function challenge_stopwatch_bingo(results) {
 function challenge_single_parkrun_count(results, shortname, longname, count) {
     complete = false
     completed_on = null
-    subparts = ['1']
+    subparts = ["1"]
     subparts_completed_count = 0
     subparts_detail = []
 
@@ -234,8 +328,6 @@ function challenge_single_parkrun_count(results, shortname, longname, count) {
             }
         }
     });
-
-    console.log(parkruns_completed)
 
     if (complete) {
         // Return all parkrun events where the limit has been reached
