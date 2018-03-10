81c91236bc6d9de6474ebb721bfa0edca704e494
fraz3alpha <andrewtaylor2@gmail.com>
2018-03-10 14:39:06 +0000

    Link to options page from help page

--- browser-extensions/chrome/js/options.js
+++ browser-extensions/chrome/js/options.js
@@ -1,12 +1,9 @@
+
 // Saves options to chrome.storage
 function save_options() {
   var athlete_number = document.getElementById('athlete_number').value;
-  var home_parkrun = document.getElementById('home_parkrun').value;
-  var is_pedant = document.getElementById('is_pedant').checked;
   chrome.storage.sync.set({
     athlete_number: athlete_number,
-    home_parkrun: home_parkrun,
-    is_pedant: is_pedant
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -22,13 +19,9 @@ function save_options() {
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
-    athlete_number: '',
-    home_parkrun: '',
-    is_pedant: false
+    athlete_number: ''
   }, function(items) {
     document.getElementById('athlete_number').value = items.athlete_number;
-    document.getElementById('home_parkrun').value = items.home_parkrun;
-    document.getElementById('is_pedant').checked = items.is_pedant;
   });
 
   // Load the GEO data with a timeout
