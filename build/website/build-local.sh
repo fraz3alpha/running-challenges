c3cbaf564cd4bc1db928252e47da8ebceda2682b
fraz3alpha <andrewtaylor2@gmail.com>
2018-04-28 15:38:10 +0100

    Fix website build after moving flag locations

--- build/website/build-local.sh
+++ build/website/build-local.sh
@@ -13,7 +13,7 @@ cp -r images/badges/256x256/*.png website/img/badges/
 
 echo "Copying flags to website img/flags directory"
 mkdir -p website/img/flags
-cp -r images/flags/png/*.png website/img/flags/
+cp -r images/flags/twemoji/png/*.png website/img/flags/
 
 echo "Copying logos to website img/logo directory"
 mkdir -p website/img/logo
