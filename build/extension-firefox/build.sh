4bb6b645d0445799d7bb48c0b70f9191df0f2560
fraz3alpha <andrewtaylor2@gmail.com>
2018-07-10 22:06:43 +0100

    Basic Firefox build
      - Build the extension to a different directory and replace
        instances of chrome-extension in url links with moz-extension

---
+++ build/extension-firefox/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -xe
+
+# Create empty build directory
+export TMP_BUILD_DIR=browser-extensions/firefox/build
+rm -rf ${TMP_BUILD_DIR} && mkdir -p ${TMP_BUILD_DIR}
+
+# Copy the images that need to be bundled
+mkdir -p ${TMP_BUILD_DIR}/images
+
+mkdir ${TMP_BUILD_DIR}/images/badges
+cp -r images/badges/256x256/*.png ${TMP_BUILD_DIR}/images/badges/
+
+mkdir ${TMP_BUILD_DIR}/images/flags
+cp -r images/flags/twemoji/png/*.png ${TMP_BUILD_DIR}/images/flags/
+
+cp -r images/logo ${TMP_BUILD_DIR}/images/
+
+# Copy the code
+cp -r browser-extensions/chrome/js ${TMP_BUILD_DIR}/
+cp -r browser-extensions/chrome/html ${TMP_BUILD_DIR}/
+cp -r browser-extensions/chrome/css ${TMP_BUILD_DIR}/
+
+# Replace all instances of "chrome-extension://" with "moz-extension://" for
+# Firefox compatibility in css files
+find ${TMP_BUILD_DIR}/ -type f -name "*.css" -exec sed -i "" "s/chrome-extension/moz-extension/g" {} \;
+
+
+# Copy the metadata
+cp -r browser-extensions/chrome/manifest.json ${TMP_BUILD_DIR}/
+
+# Move into the build directory and package everything up
+cd ${TMP_BUILD_DIR}
+zip -r extension.zip js/ html/ images/ css/ manifest.json
