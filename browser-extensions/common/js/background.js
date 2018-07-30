8142d14e0d0669f947d826fde375f2a69791dd78
fraz3alpha <andrewtaylor2@gmail.com>
2018-07-30 20:50:33 +0100

    Add testing page and improve cache error handling
      - testing.html allows you to view and modify the cache
        state and actions that can be performed, enabling you to
        attempt to test what happens in error scenarios

--- browser-extensions/common/js/background.js
+++ browser-extensions/common/js/background.js
@@ -36,6 +36,7 @@ var cache = {
         'max_age': 3 * 24 * 60 * 60 * 1000,
         'url': "https://www.parkrun.org.uk/wp-content/themes/parkrun/xml/geo.xml",
         'datatype': 'xml',
+        'enabled': true,
         'timeout': 5000
     },
     'technical_event_information': {
@@ -46,10 +47,56 @@ var cache = {
         'max_age': 3 * 24 * 60 * 60 * 1000,
         'url': "https://wiki.parkrun.com/index.php/Technical_Event_Information",
         'datatype': 'html',
+        'enabled': true,
         'timeout': 5000
     },
     'data': undefined,
     'updated_at': undefined
+}
+
+function get_cache_summary() {
+  if (cache.data === undefined) {
+    return {"error": "<no cache data>"}
+  }
+
+  var summary = {
+    'regions': '<missing>',
+    'events': '<missing>',
+    'countries': '<missing>',
+    'event_status': '<missing>'
+  }
+
+  if ('regions' in cache.data) {
+    summary.regions = Object.keys(cache.data.regions).length
+  }
+  if ('events' in cache.data) {
+    summary.events = Object.keys(cache.data.events).length
+  }
+  if ('countries' in cache.data) {
+    summary.countries = Object.keys(cache.data.countries).length
+  }
+  if ('event_status' in cache.data) {
+    if (cache.data.event_status !== undefined) {
+      summary.event_status = Object.keys(cache.data.event_status).length
+    }
+  }
+
+  return summary
+
+}
+
+function clear_cache() {
+  clear_cache_by_name("geo")
+  clear_cache_by_name("technical_event_information")
+}
+
+function clear_cache_by_name(name) {
+  if (name in cache) {
+    cache[name].raw_data = undefined
+    cache[name].updated_at = undefined
+    cache[name].last_update_attempt = undefined
+    regenerate_cache_data()
+  }
 }
 
 function traverse_geo_data(geo_data, region_name, depth=0) {
@@ -98,6 +145,13 @@ function parse_geo_data_regions(geo_data, geo_xml) {
     // console.log('parse_geo_data_regions()')
     // console.log(geo_xml)
 
+    if (geo_data === undefined) {
+      return
+    }
+    if (geo_xml === undefined) {
+      return
+    }
+
     // Find all the regions
     $(geo_xml).find('r').each(function(region_index) {
         this_region = $(this)
@@ -136,14 +190,17 @@ function parse_geo_data_regions(geo_data, geo_xml) {
     //     }
     // })
 
-    // console.log(geo_data)
-
-    return geo_data
+    return
 }
 
 function parse_geo_data_events(geo_data, geo_xml) {
 
-    // console.log('parse_geo_data_events()')
+  if (geo_data === undefined) {
+    return
+  }
+  if (geo_xml === undefined) {
+    return
+  }
 
     // Find all the events
     $(geo_xml).find('e').each(function(region_index) {
@@ -162,40 +219,43 @@ function parse_geo_data_events(geo_data, geo_xml) {
             "country_name": "unknown"
         }
     })
-    return geo_data
+
+    return
 
 }
 
-function compute_geo_data_heirachy(geo_data) {
+function compute_geo_data_heirachy(data) {
 
-    // console.log('compute_geo_data_heirachy()')
+  if (data === undefined) {
+    return
+  }
 
     // Create maps between ids and names
     region_id_to_name_map = {}
     region_name_to_id_map = {}
-    $.each(geo_data.regions, function(region_name, region_info) {
+    $.each(data.regions, function(region_name, region_info) {
         region_id_to_name_map[region_info.id] = region_name
         region_name_to_id_map[region_name] = region_info.id
     })
 
     // Add region as a child to its parent
-    $.each(geo_data.regions, function(region_name, region_info) {
+    $.each(data.regions, function(region_name, region_info) {
         if (region_info.parent_id !== null && region_info.parent_id != "") {
             if (region_info.parent_id in region_id_to_name_map) {
                 parent_region_name = region_id_to_name_map[region_info.parent_id]
-                geo_data.regions[parent_region_name].child_region_ids.push(region_info.id)
-                geo_data.regions[parent_region_name].child_region_names.push(region_info.name)
+                data.regions[parent_region_name].child_region_ids.push(region_info.id)
+                data.regions[parent_region_name].child_region_names.push(region_info.name)
             }
         }
     })
 
 
     // Find all the countries in the regions we have parsed
-    $.each(geo_data.regions, function(region_name, region_info) {
+    $.each(data.regions, function(region_name, region_info) {
         // If the country's parent id is 1, that means it is directly
         // listed under "World"
         if (region_info.parent_id == "1") {
-            geo_data.countries[region_name] = {
+            data.countries[region_name] = {
                 "name": region_name,
                 "region_name": region_name,
                 "region_id": region_info.id
@@ -204,15 +264,15 @@ function compute_geo_data_heirachy(geo_data) {
     })
 
     // Add each event to a region
-    $.each(geo_data.events, function(event_name, event_info) {
+    $.each(data.events, function(event_name, event_info) {
         if (event_info.region_id in region_id_to_name_map) {
             // Add the event under the region to which it belongs...
             // ... but only if it is a live event
             if (event_info.status === 'unknown' || event_info.status == 'Live') {
                 var event_region_name = region_id_to_name_map[event_info.region_id]
                 event_info.region_name = event_region_name
-                geo_data.regions[event_region_name].child_event_ids.push(event_info.id)
-                geo_data.regions[event_region_name].child_event_names.push(event_info.name)
+                data.regions[event_region_name].child_event_ids.push(event_info.id)
+                data.regions[event_region_name].child_event_names.push(event_info.name)
            } else {
                // console.log("Skipping "+event_info.name+" as it is in state "+event_info.status)
            }
@@ -222,53 +282,60 @@ function compute_geo_data_heirachy(geo_data) {
     })
 
     // Traverse the tree of regions from World down, and sum up all
-    // the events and ids
-    traverse_geo_data(geo_data, "World")
+    // the events and ids, but only if it exists in the data we have
+    if ("World" in data.regions) {
+      traverse_geo_data(data, "World")
+    }
 
     // Iterate though each country and set an event's country
-    $.each(geo_data.countries, function(index, country_info) {
-        $.each(geo_data.regions[country_info.name].child_event_recursive_names, function(index, event_name) {
-            geo_data.events[event_name].country_name = country_info.name
-            // console.log(geo_data.regions[country_info.name].url)
-            geo_data.events[event_name].local_url = geo_data.regions[country_info.name].url
+    $.each(data.countries, function(index, country_info) {
+        $.each(data.regions[country_info.name].child_event_recursive_names, function(index, event_name) {
+            data.events[event_name].country_name = country_info.name
+            // console.log(data.regions[country_info.name].url)
+            data.events[event_name].local_url = data.regions[country_info.name].url
         })
     })
 
-    return geo_data
+    return data
 }
 
 function parse_tee_data_event_status(data, result) {
 
-    // console.log('parse_tee_data_event_status()')
-    if (result !== undefined) {
-      // Reset the event status data to a blank map
-      data.event_status = {}
-      $(result).find('div[id=mw-content-text]>table:first').each(function(table_index) {
-         var content_table = $(this)
+  if (result === undefined) {
+    return
+  }
 
-         content_table.find('tbody>tr').each(function(row_index) {
-             var content_table_row_cell = $('td', this)
-             if (content_table_row_cell[0] !== undefined) {
-                 var parkrun_info = {
-                     parkrun_name: content_table_row_cell[0].innerText.trim(),
-                     parkrun_event_director: content_table_row_cell[1].innerText.trim(),
-                     parkrun_event_number: content_table_row_cell[2].innerText.trim(),
-                     parkrun_status: content_table_row_cell[3].innerText.trim(),
-                     parkrun_country: content_table_row_cell[4].innerText.trim(),
-                     parkrun_portal_number: content_table_row_cell[5].innerText.trim()
-                 }
-                 data.event_status[parkrun_info.parkrun_event_number] = parkrun_info
-                 // console.log(parkrun_info)
+  // Reset the event status data to a blank map
+  data.event_status = {}
+  $(result).find('div[id=mw-content-text]>table:first').each(function(table_index) {
+     var content_table = $(this)
+
+     content_table.find('tbody>tr').each(function(row_index) {
+         var content_table_row_cell = $('td', this)
+         if (content_table_row_cell[0] !== undefined) {
+             var parkrun_info = {
+                 parkrun_name: content_table_row_cell[0].innerText.trim(),
+                 parkrun_event_director: content_table_row_cell[1].innerText.trim(),
+                 parkrun_event_number: content_table_row_cell[2].innerText.trim(),
+                 parkrun_status: content_table_row_cell[3].innerText.trim(),
+                 parkrun_country: content_table_row_cell[4].innerText.trim(),
+                 parkrun_portal_number: content_table_row_cell[5].innerText.trim()
              }
-         })
-      })
-    }
+             data.event_status[parkrun_info.parkrun_event_number] = parkrun_info
+             // console.log(parkrun_info)
+         }
+     })
+  })
 
-     return data
+ return
 
 }
 
 function compute_event_status(data) {
+
+  if (data === undefined) {
+    return
+  }
 
   // Loop through the existing geo_data, and supplement it with the
   // extra event data we have found if there is a match
@@ -281,7 +348,7 @@ function compute_event_status(data) {
      }
   })
 
-  return data
+  return
 
 }
 
@@ -300,10 +367,12 @@ function get_geo_data(notify_func, freshen=false) {
         // console.log('.ajax - '+page+' - freshen='+freshen)
         // Check if see if the data is:
         // not yet available (1), or never updated (2), or expired (3), or we want a fresh copy (4)
-        if (cache[page].raw_data === undefined || // 1
+        if (cache[page].enabled && (
+            cache[page].raw_data === undefined || // 1
             cache[page].updated_at === undefined || // 2
             cache[page].updated_at < (now - cache[page].max_age) || // 3
             freshen // 4
+            )
         ) {
             update_needed = true
             // console.log('.ajax - '+page+' update needed')
@@ -341,13 +410,6 @@ function get_geo_data(notify_func, freshen=false) {
         // console.log('Updated required, executing deferred AJAX requests')
         $.when( ajax_calls[0], ajax_calls[1] ).done(
             function ( data_geo, data_tee ) {
-                // console.log('Processing returned data')
-
-                // For testing, we can wipe these
-                // data_geo = undefined
-                // cache.geo.raw_data = undefined
-                // data_tee = undefined
-                // cache.technical_event_information.raw_data = undefined
 
                 // We absolutely need the geo data, without which we can't do
                 // anything.
@@ -372,36 +434,7 @@ function get_geo_data(notify_func, freshen=false) {
                     data_tee = cache.technical_event_information.raw_data
                 }
 
-                // Build up our new data
-                var data = {
-                  'regions': {},
-                  'events': {},
-                  'countries': {},
-                  'event_status': undefined
-                }
-
-                // First of all lets go and parse all the regions and all of
-                // the events in the XML file and put them in out data structure.
-                // Any additional parsing or additions to this data will be
-                // done afterwards
-                parse_geo_data_regions(data, data_geo)
-                parse_geo_data_events(data, data_geo)
-
-                // If the technical event information has been obtained, then
-                // lets parse that.
-                // if (data_tee !== null) {
-                parse_tee_data_event_status(data, data_tee)
-                // }
-
-                // This could potentially do nothing if no event info is available
-                compute_event_status(data)
-                // console.log(data)
-                // Create the heirachy of events by region
-                compute_geo_data_heirachy(data)
-
-                // Update the global cache
-                cache.data = data
-                cache.updated_at = now
+                update_cache_data(data_geo, data_tee)
 
                 notify_geo_data(notify_func)
                 return
@@ -412,6 +445,53 @@ function get_geo_data(notify_func, freshen=false) {
         console.log('Returning cached data for TEE & Geo Data')
         notify_geo_data(notify_func)
     }
+
+}
+
+function regenerate_cache_data() {
+  update_cache_data(cache.geo.raw_data, cache.technical_event_information.raw_data)
+}
+
+function update_cache_data(data_geo, data_tee) {
+
+  if (data_geo === undefined) {
+    cache.data = undefined
+    cache.updated_at = undefined
+    return cache.data
+  }
+
+  // Build up our new data
+  var data = {
+    'regions': {},
+    'events': {},
+    'countries': {},
+    'event_status': undefined
+  }
+
+  // First of all lets go and parse all the regions and all of
+  // the events in the XML file and put them in out data structure.
+  // Any additional parsing or additions to this data will be
+  // done afterwards
+  parse_geo_data_regions(data, data_geo)
+  parse_geo_data_events(data, data_geo)
+
+  // If the technical event information has been obtained, then
+  // lets parse that.
+  // if (data_tee !== null) {
+  parse_tee_data_event_status(data, data_tee)
+  // }
+
+  // This could potentially do nothing if no event info is available
+  compute_event_status(data)
+  // console.log(data)
+  // Create the heirachy of events by region
+  compute_geo_data_heirachy(data)
+
+  // Update the global cache
+  cache.data = data
+  cache.updated_at = new Date()
+
+  return data
 
 }
 
@@ -456,5 +536,63 @@ function notify_geo_data(f) {
         // Indicate we are going to return a response asynchronously
         // https://developer.chrome.com/extensions/runtime#event-onMessage
         return true
+      }
+      if (request.action) {
+        var done = false
+        var msg = request.action + ": OK"
+        switch(request.action) {
+          case "cache-all-clear":
+            clear_cache()
+            done = true
+            break
+          case "cache-geo-clear":
+            clear_cache_by_name("geo")
+            done = true
+            break
+          case "cache-tei-clear":
+            clear_cache_by_name("technical_event_information")
+            done = true
+            break
+          case "cache-get":
+            if (cache.data) {
+              msg = JSON.stringify(cache.data,null,2)
+            } else {
+              msg = "<no cached data>"
+            }
+            done = true
+            break
+          case "cache-get-summary":
+            var cache_summary = get_cache_summary()
+            msg = JSON.stringify(cache_summary,null,2)
+            done = true
+            break
+          case "enable-geo":
+            cache.geo.enabled = true
+            done = true
+            break
+          case "disable-geo":
+            cache.geo.enabled = false
+            done = true
+            break
+          case "enable-tei":
+            cache.technical_event_information.enabled = true
+            done = true
+            break
+          case "disable-tei":
+            cache.technical_event_information.enabled = false
+            done = true
+            break
+        }
+        if (done) {
+          sendResponse({
+            "action": request.action,
+            "msg": msg
+          });
+        } else {
+          sendResponse({
+            "action": request.action,
+            "msg": request.action + ": Unsupported action"
+          });
+        }
       }
     });
