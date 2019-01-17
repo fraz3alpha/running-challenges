be98c8095c0e7aee5ce52a2531f0444b5fd07bd0
Jason <jgossit@gmail.com>
2019-01-17 19:15:45 +1100

    habitual challenge

--- browser-extensions/common/js/lib/challenges.js
+++ browser-extensions/common/js/lib/challenges.js
@@ -84,6 +84,10 @@ function generate_running_challenge_data(data) {
       "shortname": "groundhog-day",
       "name": "Groundhog Day",
       "help": "Finish with the same time at the same parkrun location on two consecutive parkruns."}))
+    challenge_data.push(challenge_habitual(data, {
+      "shortname": "habitual",
+      "name": "Habitual",
+      "help": "Finish in the same position on 10 occasions."}))
     challenge_data.push(challenge_on_dates(data, {
       "shortname": "all-weather-runner",
       "name": "All Weather Runner",
@@ -1923,6 +1927,58 @@ function challenge_groundhog_day(data, params) {
     }
 
     // Return an object representing this challenge
+    return update_data_object(o)
+}
+
+function challenge_habitual(data, params) {
+    var parkrun_results = data.parkrun_results
+    var o = create_data_object(params, "runner")
+    o.complete = false
+    o.subparts = ["1"]
+    o.summary_text = "0"
+    var positionCounts = {}
+    parkrun_results.forEach(function (parkrun_event) {
+        if (o.subparts_detail.length == 0) {
+            var positionCount = positionCounts[parkrun_event.position]
+            if (positionCount === undefined)
+                positionCounts[parkrun_event.position] = 1
+            else
+                positionCounts[parkrun_event.position] = positionCount + 1
+            if (positionCount == 9) { // now 10
+                o.subparts_detail.push({
+                    "name": "Pos " + parkrun_event.position,
+                    "date": parkrun_event.date,
+                    "info": parkrun_event.date,
+                    "subpart": "10x"
+                })
+                o.subparts_completed_count = 1
+                o.summary_text = "1"
+                o.complete = true
+                o.completed_on = parkrun_event.date
+            }
+        }
+    })
+    
+    var highestCount = 0
+    var highestPosition = 0
+    if (!o.complete) {
+        for (var position in positionCounts) {
+            if (positionCounts[position] > highestCount) {
+                highestCount = positionCounts[position]
+                highestPosition = position
+            }
+        }
+    }
+
+    if (o.subparts_detail.length == 0) {
+        o.subparts_detail.push({
+            "name": "Pos " + highestPosition,
+            "subpart": highestCount + "x",
+            "date": "-",
+            "info": "-"
+        })
+    }
+    
     return update_data_object(o)
 }
 
