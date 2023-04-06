fab40d86a365e1d9ac85dfbf6eebfafb30436733
Andy Taylor <andrewtaylor2@gmail.com>
2023-04-06 21:31:51 +0100

    Updates to include japan

--- browser-extensions/common/js/tests/test-selenium/update.sh
+++ browser-extensions/common/js/tests/test-selenium/update.sh
@@ -1,11 +1,11 @@
 export USER_AGENT='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
 export TARGET_ROOT_DIR="`pwd`/mock-parkrun-sites/sites"
 
-curl -H "user-agent: ${USER_AGENT}" https://www.parkrun.org.uk/parkrunner/1309364/all/ -o "${TARGET_ROOT_DIR}/parkrun.org.uk/contents/parkrunner/1309364/all/index.html"
-curl -H "user-agent: ${USER_AGENT}" https://www.parkrun.org.uk/parkrunner/1309364/ -o "${TARGET_ROOT_DIR}/parkrun.org.uk/contents/parkrunner/1309364/index.html"
+declare -a PARKRUN_HOSTNAMES=( "parkrun.org.uk" "parkrun.com.de" "parkrun.pl" "parkrun.jp" )
 
-curl -H "user-agent: ${USER_AGENT}" https://www.parkrun.com.de/parkrunner/1309364/all/ -o "${TARGET_ROOT_DIR}/parkrun.com.de/contents/parkrunner/1309364/all/index.html"
-curl -H "user-agent: ${USER_AGENT}" https://www.parkrun.com.de/parkrunner/1309364/ -o "${TARGET_ROOT_DIR}/parkrun.com.de/contents/parkrunner/1309364/index.html"
-
-curl -H "user-agent: ${USER_AGENT}" https://www.parkrun.pl/parkrunner/1309364/all/ -o "${TARGET_ROOT_DIR}/parkrun.pl/contents/parkrunner/1309364/all/index.html"
-curl -H "user-agent: ${USER_AGENT}" https://www.parkrun.pl/parkrunner/1309364/ -o "${TARGET_ROOT_DIR}/parkrun.pl/contents/parkrunner/1309364/index.html"
+for PARKRUN_HOSTNAME in "${PARKRUN_HOSTNAMES[@]}"
+do
+    mkdir -p "${TARGET_ROOT_DIR}/${PARKRUN_HOSTNAME}/contents/parkrunner/1309364/all/"
+    curl -H "user-agent: ${USER_AGENT}" https://www.${PARKRUN_HOSTNAME}/parkrunner/1309364/all/ -o "${TARGET_ROOT_DIR}/${PARKRUN_HOSTNAME}/contents/parkrunner/1309364/all/index.html"
+    curl -H "user-agent: ${USER_AGENT}" https://www.${PARKRUN_HOSTNAME}/parkrunner/1309364/ -o "${TARGET_ROOT_DIR}/${PARKRUN_HOSTNAME}/contents/parkrunner/1309364/index.html"
+done
