16ac4097f23ff300a39981c0e926d325b28806a7
Andy Taylor <andrewtaylor2@gmail.com>
2023-06-29 22:00:56 +0100

    Add test and supporting files for parkrunner with no results

--- browser-extensions/common/js/tests/ui-test/tests/extension.spec.js
+++ browser-extensions/common/js/tests/ui-test/tests/extension.spec.js
@@ -45,6 +45,26 @@ test('Basic extension load test', async ({ page }) => {
 
   console.log(`Expecting the extension to have been loaded from ${extensionPath}`)
 
+  await page.goto(`https://www.${countryDomain}/parkrunner/999999/all/`);
+
+  // Wait 3 seconds, this should be plenty as we are serving all the data locally and there shoudn't be
+  // any internet calls
+  await page.waitForTimeout(3000);
+
+  // This takes a screenshot of the entire page, which is probably a good idea to do early on,
+  // but we should really wait until the extension has loaded.
+  await page.screenshot({ path: 'screenshot.png', fullPage: true });
+
+  let messagesDiv = page.locator("#running_challenges_messages_div")
+
+  await expect(messagesDiv).toHaveText("Additional badges provided by Running Challenges", {timeout: 10000})
+
+});
+
+test('No results for parkrunner load test', async ({ page }) => {
+
+  console.log(`Expecting the extension to have been loaded from ${extensionPath}`)
+
   await page.goto(`https://www.${countryDomain}/parkrunner/1309364/all/`);
 
   // Wait 3 seconds, this should be plenty as we are serving all the data locally and there shoudn't be
