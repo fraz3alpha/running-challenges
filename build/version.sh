b20126c9203a2696c33e97e5eba13bfe77fd3097
Pete Johns <paj+github@johnsy.com>
2024-10-26 00:36:12 +1100

    mv3

--- build/version.sh
+++ build/version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-export VERSION=1.4.0
+export VERSION=2.0.0
 # Strip any 'v' characters from the version string, as exist in the git tag
 export EXTENSION_BUILD_VERSION=`echo ${EXTENSION_BUILD_VERSION:-$VERSION} | sed -e s/v//`
 # Default the build id variable to zero if not set - The CI system should set this to
